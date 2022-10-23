aef008cac38d16c0b4e128f13af4df9fbb61e644
GarryLaly <garrylaly@gmail.com>
2022-10-23 13:58:47 +0700

    Initial commit and ready for use

---
+++ run_me.py
@@ -0,0 +1,17 @@
+from Detector import *
+
+# modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
+modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d4_coco17_tpu-32.tar.gz"
+
+classFile = "coco.names"
+imagePath = "test/3.jpg"
+# videoPath = "test/video2.mp4"
+videoPath = 0  # For Webcam
+threshold = 0.5
+
+detector = Detector()
+detector.readClasses(classFile)
+detector.downloadModel(modelURL)
+detector.loadModel()
+detector.predictImage(imagePath, threshold)
+# detector.predictVideo(videoPath, threshold)
